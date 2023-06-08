49956e722da0c3205197d5186521a05e92dfab32
osmon23 <oma_23_03_98@mail.ru>
2023-06-08 17:41:31 +0600

    add models and serializers

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Message(models.Model):
+    text = models.TextField(
+        _('Текст'),
+        null=True,
+        blank=True,
+    )
+    time = models.DateTimeField(
+        _('Время'),
+        auto_now_add=True,
+    )
+
+    def __str__(self):
+        return f'{self.time}: {self.text}'
+
+    class Meta:
+        verbose_name = _('Сообщение')
+        verbose_name_plural = _('Сообщения')
--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
